2ebc5d46456620ddf9b1f3bbcddab45a9f2edb56
Eduardo Fermiano <eduardo.fermiano@kitado.com.br>
2017-04-11 15:46:22 -0300

    Segregating the api methods to a controller and validating them

--- node_api/controllers/userController.js
+++ node_api/controllers/userController.js
@@ -1,47 +1,86 @@
+'use strict';
 
+const helpers = require('../config/helperFunctions');
+
+//Fake Database
+
+let users = {};
+let max_users_id = 0;
 
 module.exports = function(server){
 
     server.get('/', (req, res, next)=>{
-        success(res, next, users);
+        helpers.success(res, next, users);
     });
 
     server.get('/users/:id', (req, res, next)=>{
-        if(typeof (users[req.params.id]) === 'undefined'){
-            failure(res, next, 'The user could not be found on the database', 404);
+
+        req.assert('id', 'Id is required and must be numeric').notEmpty().isInt();
+        let errors = req.validationErrors();
+
+        if(errors){
+            helpers.failure(res, next, errors[0], 400);
         }
-        success(res, next, users[parseInt(req.params.id)]);
+
+        if(typeof (users[req.params.id]) === 'undefined'){
+            helpers.failure(res, next, 'The user could not be found on the database', 404);
+        }
+        helpers.success(res, next, users[parseInt(req.params.id)]);
     });
 
     server.post('/users', (req, res, next)=>{
+
+        req.assert('first_name', 'First name is a required parameter').notEmpty();
+        req.assert('last_name', 'Last name is a required parameter').notEmpty();
+        req.assert('user_email', 'Email must not be empty and must be a valid one').notEmpty().isEmail();
+        req.assert('career', 'Must be one of the options: proggrammer, DBA, analyst').isIn(['proggrammer', 'DBA', 'analyst']);
+
+        let errors = req.validationErrors();
+
+        if(errors){
+            helpers.failure(res, next, errors, 400);
+        }
 
         let user = req.params;
         max_users_id++;
         user.id = max_users_id;
         users[user.id] = user;
-        success(res, next, users);
+        helpers.success(res, next, users);
 
     });
 
     server.put('/users/:id', (req, res, next)=>{
+
+        req.assert('id', 'Id is required and must be numeric').notEmpty().isInt();
+        let errors = req.validationErrors();
+
+        if(errors){
+            helpers.failure(res, next, errors[0], 400);
+        }
         if(typeof (users[req.params.id]) === 'undefined'){
-            failure(res, next, 'The user could not be found on the database', 404);
+            helpers.failure(res, next, 'The user could not be found on the database', 404);
         }
         let user = users[parseInt(req.params.id)];
         let updates = req.params;
         for (let field in updates){
             user[field] = updates[field];
         }
-        success(res, next, user);
+        helpers.success(res, next, user);
 
     });
 
     server.del('/users/:id', (req, res, next)=>{
+        req.assert('id', 'Id is required and must be numeric').notEmpty().isInt();
+        let errors = req.validationErrors();
+
+        if(errors){
+            helpers.failure(res, next, errors[0], 400);
+        }
         if(typeof (users[req.params.id]) === 'undefined'){
-            failure(res, next, 'The user could not be found on the database', 404);
+            helpers.failure(res, next, 'The user could not be found on the database', 404);
         }
         delete users[parseInt(req.params.id)];
-        success(res, next, []);
+        helpers.success(res, next, []);
 
     });
 };
